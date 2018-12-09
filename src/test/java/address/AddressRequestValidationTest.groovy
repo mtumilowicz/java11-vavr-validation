1b65145f3a385e1ade3e9526c86e0195088211b3
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-12-09 19:10:28 +0100

    AddressRequestValidationTest.groovy

---
+++ src/test/java/address/AddressRequestValidationTest.groovy
@@ -0,0 +1,45 @@
+package address
+
+import io.vavr.collection.List
+import patterns.PostalCode
+import patterns.Word
+import spock.lang.Specification
+
+/**
+ * Created by mtumilowicz on 2018-12-09.
+ */
+class AddressRequestValidationTest extends Specification {
+
+    def "valid AddressRequest"() {
+        given:
+        def request = AddressRequest.builder()
+                .city("Olkusz")
+                .postalCode("32-300")
+                .build()
+
+        when:
+        def report = AddressRequestValidation.validate(request)
+
+        then:
+        report.isValid()
+        report.get() == ValidAddressRequest.builder()
+                .city(Word.of("Olkusz"))
+                .postalCode(PostalCode.of("32-300"))
+                .build()
+    }
+
+    def "not valid AddressRequest"() {
+        given:
+        def request = AddressRequest.builder()
+                .city("wrong!")
+                .postalCode("wrong!")
+                .build()
+
+        when:
+        def report = AddressRequestValidation.validate(request)
+
+        then:
+        report.isInvalid()
+        report.getError() == ["wrong! is not a proper word!", "wrong! is not a proper postal code!"] as List
+    }
+}
